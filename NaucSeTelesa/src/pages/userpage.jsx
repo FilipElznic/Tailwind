bfd8b2a924a06b61c5f7cfeac866ae8cd7664b4e
Filip Elznic <elznicfilip@gmail.com>
2024-12-03 10:03:37 +0100

    Update userpage.jsx

--- NaucSeTelesa/src/pages/userpage.jsx
+++ NaucSeTelesa/src/pages/userpage.jsx
@@ -4,35 +4,31 @@ import "../App.css";
 function UserPage() {
   return (
     <>
-      <Navbar />
       <div className="relative h-screen bg-color">
         {/* Centered Video */}
-        <div className="flex justify-center items-center h-full">
-          <video className="w-full h-auto" autoPlay loop muted playsInline>
-            <source src="/robot.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
 
         {/* Grid container */}
-        <div
-          className="
-            grid 
-            grid-cols-[repeat(2,0.5fr)_1fr_repeat(2,0.5fr)] 
-            grid-rows-[0.8fr_repeat(4,1fr)] 
-            gap-0 absolute top-0 left-0 w-full h-full z-10
-          "
-        >
-          {/* Div 1 */}
-          <div className="bg-gray-700 sm:col-start-1 sm:col-end-3 sm:row-start-5 sm:row-end-6 rounded-tr-xl">
-            Div 1
+        <div className="grid grid-cols-5 grid-rows-5 gap-0">
+          <div className="col-start-2 col-end-6 row-start-2 row-end-6 bg-black">
+            <div className="flex justify-center items-center h-full">
+              <video className="w-full h-auto" autoPlay loop muted playsInline>
+                <source src="/robot.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
-          {/* Div 2 */}
-          <div className="bg-gray-700 sm:col-start-4 sm:col-end-6 sm:row-start-1 sm:row-end-2 rounded-bl-xl">
-            Div 2
+          <div className="col-start-1 col-end-2 row-start-2 row-end-6 bg-black">
+            <div className="flex flex-col justify-center h-1/2">
+              <h1 className="text-white">Nechte se vnést do světa geometrie</h1>
+              <p className="text-white">
+                Pokud by se pro stejný účel použil smysluplný text, bylo by
+                těžké hodnotit pouze vzhled, aniž by se pozorovatel nechal svést
+                ke čtení obsahu. Pokud by byl naopak použit nesmyslný, ale
+                pravidelný text
+              </p>
+            </div>
           </div>
-          {/* Div 3 */}
-          <div className="bg-gray-700 sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-2 rounded-br-xl">
+          <div className="col-start-1 col-end-6 row-start-1 row-end-2 bg-black">
             Div 3
           </div>
         </div>
