3c60d358367d5a4dd0e7795ed32d3ebd52443d96
Filip Elznic <elznicfilip@gmail.com>
2024-12-11 20:47:20 +0100

    doplnujici form

--- NaucSeTelesa/src/pages/successPage.jsx
+++ NaucSeTelesa/src/pages/successPage.jsx
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AdminPage from "./adminPage";
+import Robot from "/robot.png";
 
 const supabase = createClient(
   "https://bviuhriolcuvayzbgzum.supabase.co",
@@ -66,16 +67,76 @@ function SuccessPage() {
 
   return (
     <>
-      <div className="min-h-screen bg-quick text-white flex flex-col items-center justify-center p-4">
-        <h1 className="text-2xl font-bold mb-6">Registrace proběhla úspěšně</h1>
-        <button
-          onClick={signOutUser}
-          className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 hover:scale-105 transform transition-all duration-200 ease-in-out focus:ring-4 focus:ring-blue-300"
-        >
-          Odhlásit se
-        </button>
+      <div className="h-screen   text-white   flex flex-col items-center justify-center p-20">
+        <div className="w-full p-5 bg-quick h-full flex flex-col items-center justify-center rounded-3xl">
+          <h2 className="text-xl  mb-6 text-gray-700">
+            Registrace proběhla úspěšně, ale ...
+          </h2>
+          <h1 className="text-6xl font-bold mb-6">
+            Ještě něco o tobě potřebujeme vědět
+          </h1>
+          <div className="h-full w-full flex flex-col sm:flex-row">
+            <div className="flex-1 flex justify-center items-center">
+              <img src={Robot} className=" object-fit" />
+            </div>
+            <div className="flex-1 flex justify-center items-center">
+              <form className="space-y-4 p-6 w-3/4 rounded-xl shadow-lg">
+                <div>
+                  <label
+                    htmlFor="jmeno"
+                    className="block text-sm text-white mb-1"
+                  >
+                    jméno
+                  </label>
+                  <input
+                    type="text"
+                    id="jmeno"
+                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="prijmeni"
+                    className="block text-sm text-white mb-1"
+                  >
+                    příjmení
+                  </label>
+                  <input
+                    type="text"
+                    id="prijmeni"
+                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="prezdivka"
+                    className="block text-sm text-white mb-1"
+                  >
+                    přezdívka
+                  </label>
+                  <input
+                    type="text"
+                    id="prezdivka"
+                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-2 rounded-full bg-purple-700 text-white font-semibold hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                >
+                  odeslat
+                </button>
+              </form>
+            </div>
+          </div>
 
-        <div className="w-[80vw] webbg h-screen"></div>
+          <button
+            onClick={signOutUser}
+            className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 hover:scale-105 transform transition-all duration-200 ease-in-out focus:ring-4 focus:ring-blue-300"
+          >
+            Odhlásit se
+          </button>
+        </div>
       </div>
     </>
   );
