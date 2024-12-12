2d85ff4860adc1664acff4909f6a2379ec668765
Filip Elznic <elznicfilip@gmail.com>
2024-12-12 14:32:22 +0100

    css styling

--- NaucSeTelesa/src/pages/successPage.jsx
+++ NaucSeTelesa/src/pages/successPage.jsx
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AdminPage from "./adminPage";
 import Robot from "/robot.png";
 
 const supabase = createClient(
@@ -67,8 +66,8 @@ function SuccessPage() {
 
   return (
     <>
-      <div className="h-screen  text-white imgbg  flex flex-col items-center justify-center p-20">
-        <div className="w-full p-5 bg-opacity-90 bg-black h-full flex flex-col items-center justify-center rounded-3xl">
+      <div className="min-h-screen  text-white imgbg flex flex-col items-center justify-center p-20 ">
+        <div className="w-full p-5 bg-opacity-90 bg-form  flex flex-col items-center justify-center rounded-3xl sm:h-[80vh]">
           <h2 className="text-xl  mb-6 text-gray-700">
             Registrace proběhla úspěšně, ale ...
           </h2>
@@ -77,10 +76,10 @@ function SuccessPage() {
           </h1>
           <div className="h-full w-full flex flex-col sm:flex-row">
             <div className="flex-1 flex justify-center items-center">
-              <img src={Robot} className=" object-fit" />
+              <img src={Robot} className=" object-cover" />
             </div>
             <div className="flex-1 flex justify-center items-center">
-              <form className="space-y-4 p-6 w-3/4 rounded-xl shadow-lg">
+              <form className="space-y-4 p-6 w-3/4 rounded-xl shadow-lg flex justify-center items-center flex-col">
                 <div>
                   <label
                     htmlFor="jmeno"
@@ -91,7 +90,7 @@ function SuccessPage() {
                   <input
                     type="text"
                     id="jmeno"
-                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    className="w-80 px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
                   />
                 </div>
                 <div>
@@ -104,7 +103,7 @@ function SuccessPage() {
                   <input
                     type="text"
                     id="prijmeni"
-                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    className="w-80 px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
                   />
                 </div>
                 <div>
@@ -117,12 +116,12 @@ function SuccessPage() {
                   <input
                     type="text"
                     id="prezdivka"
-                    className="w-full px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
+                    className="w-80 px-4 py-2 rounded-full formcolor text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
                   />
                 </div>
                 <button
                   type="submit"
-                  className="w-full py-2 rounded-full bg-purple-700 text-white font-semibold hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                  className="w-60 py-2 rounded-full bg-purple-700 text-white font-semibold hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-500"
                 >
                   odeslat
                 </button>
@@ -132,7 +131,7 @@ function SuccessPage() {
 
           <button
             onClick={signOutUser}
-            className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-500 hover:scale-105 transform transition-all duration-200 ease-in-out focus:ring-4 focus:ring-blue-300"
+            className="px-6 py-3 bg-opacity-40 bg-gray-900 text-white font-semibold rounded-lg shadow-md hover:bg-gray-900 hover:scale-105 transform transition-all duration-200 ease-in-out focus:ring-4 focus:ring-blue-300"
           >
             Odhlásit se
           </button>
