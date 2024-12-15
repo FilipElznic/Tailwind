79c4470699ae38ac31c6f16ebaa6709bdd89c237
Filip Elznic <elznicfilip@gmail.com>
2024-12-15 22:01:51 +0100

    omg

--- NaucSeTelesa/src/Components/Navbar.jsx
+++ NaucSeTelesa/src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-quick text-white px-4 py-2 flex justify-between items-center">
+    <nav className="bg-quick  text-white px-4 py-2 flex justify-between items-center">
       {/* Left Side: Collapsible Menu */}
       <div className="flex items-center">
         {/* Hamburger Icon */}
