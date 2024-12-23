64057dc0f06accf9c2648f63be51525192dcc3b4
Filip Elznic <elznicfilip@gmail.com>
2024-12-23 23:44:52 +0100

    Update userpage.jsx

--- NaucSeTelesa/src/pages/userpage.jsx
+++ NaucSeTelesa/src/pages/userpage.jsx
@@ -60,6 +60,26 @@ function UserPage() {
       )}
 
       <div className="border-t border-gray-700 webbg text-white ">
+        <div className="flex  flex-col justify-center min-h-screen text-white">
+          <h1 className="text-white text-4xl sm:text-7xl md:text-9xl flex justify-center items-center font-bold p-5">
+            OVERUSER
+          </h1>
+          <div className="flex flex-col">
+            <div className="flex flex-col items-center gap-5 mt-10 md:flex-row md:justify-center">
+              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96  bg-zinc-900 rounded-3xl"></div>
+              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl"></div>
+            </div>
+            <div className="flex flex-col items-center gap-5 mt-10 md:flex-row md:justify-center">
+              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl"></div>
+              <div className="md:w-72 md:h-60 lg:w-96  flex flex-col justify-evenly">
+                <div className="w-56 mx-5 mb-5 md:w-full md:h-full h-24 bg-zinc-900 rounded-3xl"></div>
+                <div className="w-56 mx-5 md:w-full md:h-full h-24 bg-zinc-900 rounded-3xl"></div>
+              </div>
+              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 m-5 bg-zinc-900 rounded-3xl"></div>
+            </div>
+          </div>
+        </div>
+
         <div className="lg:flex lg:flex-row lg:w-full lg:justify-evenly min-h-screen lg:items-center">
           <div className="md:full md:flex md:flex-col md:justify-center md:items-center md:mt-20 lg:flex lg:flex-col lg:items-start lg:w-1/4 ">
             {/*div pro nadpis a text*/}
@@ -108,26 +128,6 @@ function UserPage() {
           </div>
 
           <img src="/splineImg.png" />
-        </div>
-
-        <div className="flex  flex-col justify-center min-h-screen text-white">
-          <h1 className="text-white text-4xl sm:text-7xl md:text-9xl flex justify-center items-center font-bold p-5">
-            OVERUSER
-          </h1>
-          <div className="flex flex-col">
-            <div className="flex flex-col items-center gap-5 mt-10 md:flex-row md:justify-center">
-              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96  bg-black rounded-3xl"></div>
-              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 bg-black rounded-3xl"></div>
-            </div>
-            <div className="flex flex-col items-center gap-5 mt-10 md:flex-row md:justify-center">
-              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 bg-black rounded-3xl"></div>
-              <div className="md:w-72 md:h-60 lg:w-96  flex flex-col justify-evenly">
-                <div className="w-56 mx-5 mb-5 md:w-full md:h-full h-24 bg-black rounded-3xl"></div>
-                <div className="w-56 mx-5 md:w-full md:h-full h-24 bg-white rounded-3xl"></div>
-              </div>
-              <div className="w-56 h-56 md:w-72 md:h-60 lg:w-96 m-5 bg-black rounded-3xl"></div>
-            </div>
-          </div>
         </div>
       </div>
 
