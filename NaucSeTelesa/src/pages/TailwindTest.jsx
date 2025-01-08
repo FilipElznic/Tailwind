e806db9dd0e558a6ee5d406b7639344deb518e9f
Filip Elznic <elznicfilip@gmail.com>
2025-01-08 13:17:19 +0100

    Update TailwindTest.jsx

--- NaucSeTelesa/src/pages/TailwindTest.jsx
+++ NaucSeTelesa/src/pages/TailwindTest.jsx
@@ -55,8 +55,8 @@ function TailwindTest() {
         Osobní stránka
       </h1>
       <div className="flex flex-col h-full w-5/6 justify-center items-center">
-        <div className="flex flex-col sm:flex-row w-full">
-          <div className="w-full md:w-1/5 h-80 usergradient m-2 rounded-3xl justify-center items-center flex flex-col hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="w-full lg:w-1/5 h-80 usergradient m-2 rounded-3xl justify-center items-center flex flex-col hover:scale-105 transition-transform duration-300">
             <div className="w-full h-5/6 justify-center items-center flex flex-col">
               <IoShieldSharp className="w-2/3 h-5/6 relative text-zinc-900 drop-shadow-white-glow" />
 
@@ -72,26 +72,30 @@ function TailwindTest() {
             <p className="text-2xl text-white mb-3">Úroveň</p>
           </div>
 
-          <div className="w-full md:w-3/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300">
+          <div className="w-full lg:w-3/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300 text-center lg:text-start  justify-center lg:justify-start">
             <div className="flex flex-col h-full justify-center text-white p-11">
               {data ? (
                 <>
-                  <h1 className="text-7xl p-5">
+                  <h1 className="text-2xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-7xl p-5">
                     <p className="userlvl typing-animation">
                       {data.name} {data.surname}
                     </p>
                   </h1>
-                  <p className="text-2xl userid px-2">{data.authid}</p>
+                  <p className="text-md lg:text-2xl userid px-2">
+                    {data.authid}
+                  </p>
                   {authUser ? (
-                    <p className="text-3xl useremail p-2">{authUser.email}</p>
+                    <p className="text-sm sm:text-md md:text-3xl useremail p-2">
+                      {authUser.email}
+                    </p>
                   ) : (
                     <p className="text-sm">Načítám data ...</p>
                   )}
                   <div className="flex flex-col">
-                    <div className="w-3/5 flex justify-end">
-                      <p className="text-xl mr-2">{data.xp % 100} %</p>
+                    <div className="w-full lg:w-3/5 flex justify-end">
+                      <p className="text-xl ">{data.xp % 100} %</p>
                     </div>
-                    <div className="h-8 w-3/5 bg-black rounded-3xl relative border flex items-center">
+                    <div className="h-8 w-full lg:w-3/5 bg-black rounded-3xl relative border flex items-center">
                       <div
                         className="h-6 userlvl1 rounded-3xl flex items-center justify-center text-white ml-1 mr-1"
                         style={{
@@ -107,12 +111,12 @@ function TailwindTest() {
               )}
             </div>
           </div>
-          <div className="w-full md:w-1/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300">
+          <div className="w-full lg:w-1/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300">
             <Spline scene="https://prod.spline.design/i4RPN7ynugvjhc24/scene.splinecode" />
           </div>
         </div>
-        <div className="flex flex-col sm:flex-row w-full">
-          <div className="w-full md:w-2/5 h-80 usergradient m-2 rounded-3xl relative hover:scale-105 transition-transform duration-300 cursor-pointer">
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="w-full lg:w-2/5 h-80 usergradient m-2 rounded-3xl relative hover:scale-105 transition-transform duration-300 cursor-pointer">
             <Link to={"/ukoly"}>
               <img
                 src="/userimg.png"
@@ -128,9 +132,9 @@ function TailwindTest() {
             </Link>
           </div>
 
-          <div className="w-full md:w-1/5 h-80 usergradient m-2 rounded-3xl"></div>
+          <div className="w-full lg:w-1/5 h-80 usergradient m-2 rounded-3xl"></div>
 
-          <div className="w-full md:w-2/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300">
+          <div className="w-full lg:w-2/5 h-80 usergradient m-2 rounded-3xl hover:scale-105 transition-transform duration-300">
             <Link to={"/telesa"} className=" ">
               <div className="h-full w-full flex flex-col justify-center items-center text-white p-11">
                 <h1 className="text-xl md:text-5xl lg:text-5xl pb-2 w-full text-center font-semibold userlvl">
