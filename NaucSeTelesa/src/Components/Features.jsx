f1606acae63d8f72e546261df6d1b246e8bacf05
Filip Elznic <elznicfilip@gmail.com>
2025-01-13 12:27:35 +0100

    Update Features.jsx

--- NaucSeTelesa/src/Components/Features.jsx
+++ NaucSeTelesa/src/Components/Features.jsx
@@ -11,15 +11,15 @@ function Features() {
     <div className="lg:flex lg:flex-row lg:w-full lg:justify-evenly min-h-screen lg:items-center text-white">
       <div className="md:full md:flex md:flex-col md:justify-center md:items-center md:mt-20 lg:flex lg:flex-col lg:items-start lg:w-1/4 ">
         <h2
-          className="text-2xl sm:text-6xl md:text-7xl xl:text-9xl font-bold md:text-start  p-5 md:pb-20 userlvl
+          className="text-2xl sm:text-6xl md:text-7xl xl:text-9xl font-bold md:text-start text-center md:pb-20 userlvl
            "
         >
           FUNKCE WEBU
         </h2>
         <p className="text-xl xl:2xl  md:text-start p-5 ">
-          Pokud by se pro starý účel použil smysluplný text, bylo by těžké
-          hodnotit pouze umění, aniž by se posuzoval možná smysl té části
-          obsahu.
+          Následující funkce vám pomohou lépe pochopit geometrii a zlepšit vaše
+          znalosti. Díky těmto funkcím se budete moci učit efektivněji a
+          zábavněji.
         </p>
       </div>
       <div className="md:mr-8">
