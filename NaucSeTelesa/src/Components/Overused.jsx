e6ccecfcc44554c97c0ee4b1c80ef12d5dfb9424
Filip Elznic <elznicfilip@gmail.com>
2025-01-14 09:55:10 +0100

    Update Overused.jsx

--- NaucSeTelesa/src/Components/Overused.jsx
+++ NaucSeTelesa/src/Components/Overused.jsx
@@ -31,10 +31,10 @@ function Overused() {
           </div>
         </div>
         <div className="flex flex-col items-center mt-10 md:flex-row md:justify-center">
-          <div className="w-2/3 h-60 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl ">
-            <div className="w-full h-full flex flex-col justify-center items-center text-center ml-4">
+          <div className="w-2/3 h-60 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl lg:ml-4">
+            <div className="w-full h-full flex flex-col justify-center items-center text-center ">
               <h1 className="text-xl md:text-2xl lg:text-3xl mb-2 userlvl">
-                Sledujte svůj pokrok a soutěžte s ostatními
+                Sledujte svůj pokrok a soutěžte s ostatnímif
               </h1>
               <p>
                 Získejte body za splněné úkoly, zlepšujte své dovednosti a
@@ -42,11 +42,11 @@ function Overused() {
               </p>
             </div>
           </div>
-          <div className="md:w-72 md:h-60 lg:w-96 flex flex-col justify-evenly items-center mx-4">
+          <div className="md:w-72 md:h-60 lg:w-96 flex flex-col justify-evenly items-center mx-4 my-5">
             <div className="w-56 mb-5 md:w-full md:h-full h-24 bg-zinc-900 rounded-3xl"></div>
             <div className="w-56 md:w-full md:h-full h-24 bg-zinc-900 rounded-3xl"></div>
           </div>
-          <div className="w-2/3 h-60 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl ">
+          <div className="w-2/3 h-60 md:w-72 md:h-60 lg:w-96 bg-zinc-900 rounded-3xl lg:mr-4">
             <div className="w-full h-full flex flex-col justify-center items-center text-center mr-4">
               <h1 className="text-xl md:text-2xl lg:text-3xl mb-2 userlvl">
                 Sledujte svůj pokrok a soutěžte s ostatními
