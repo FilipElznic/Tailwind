20f7f5e2a3fbbdd09063f356ec67dc45fdea01ba
Filip Elznic <elznicfilip@gmail.com>
2025-01-19 23:58:15 +0100

    Update aboutPage.jsx

--- NaucSeTelesa/src/pages/aboutPage.jsx
+++ NaucSeTelesa/src/pages/aboutPage.jsx
@@ -1,8 +1,18 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
+
 function AboutPage() {
   return (
-    <div>
-      <h1>About Page</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-center animate-pulse">
+          About Page soon
+        </h1>
+      </div>
+      <Footer />
+    </>
   );
 }
 
