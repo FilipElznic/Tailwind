7b2040f7b88bfc2514e2f81672b451e3b60932eb
Filip Elznic <elznicfilip@gmail.com>
2025-01-27 10:50:50 +0100

    Update aboutPage.jsx

--- NaucSeTelesa/src/pages/aboutPage.jsx
+++ NaucSeTelesa/src/pages/aboutPage.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Spline from "@splinetool/react-spline";
 
 function AboutPage() {
   return (
     <>
       <Navbar />
-      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-white">
+      <div className="flex items-center justify-center min-h-screen bg-black text-white">
         <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold text-center animate-pulse">
           About Page soon
         </h1>
+
+        <Spline scene="https://prod.spline.design/8yHWaHayhScHm75a/scene.splinecode" />
       </div>
       <Footer />
     </>
