c0109cd111d92c87bc7908bc43d385944b0e6589
Filip Elznic <elznicfilip@gmail.com>
2025-02-11 12:05:19 +0100

    Update LeaderboardW.jsx

--- NaucSeTelesa/src/Components/LeaderboardW.jsx
+++ NaucSeTelesa/src/Components/LeaderboardW.jsx
@@ -5,16 +5,15 @@ function LeaderboardW() {
     <>
       <div className="flex flex-col h-full w-full justify-center items-center min-w-screen">
         <div className="w-5/6">
-          <div className="flex flex-col lg:flex-row w-full">
+          <div className="flex flex-col lg:flex-row w-full h-full">
             <div className="w-full lg:w-3/5 h-full usergradient m-2 rounded-3xl relative hover:scale-105 transition-transform duration-300 ">
-              <h1 className="text-white text-3xl md:text-4xl lg:text-6xl p-10">
+              <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl p-10 userlvl">
                 Žebříček uživatelů
               </h1>
-              <p className="text-xl md:text-2xl text-white w-full md:w-2/3 mt-10">
+              <p className="text-xl md:text-2xl text-white w-full md:w-2/3 my-10 p-5 mb-16">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos
                 enim cumque fugit repellat commodi, quod, praesentium, veniam
-                saepe a ea temporibus ratione? Temporibus, provident pariatur
-                maxime nisi dolores animi recusandae.
+                saepe a ea temporibus ratione?
               </p>
             </div>
 
