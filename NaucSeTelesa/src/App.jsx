e68314db4863b9f7341c0c41b5b46709417e6c10
Filip Elznic <elznicfilip@gmail.com>
2025-02-13 14:05:29 +0100

    minus minus

--- NaucSeTelesa/src/App.jsx
+++ NaucSeTelesa/src/App.jsx
@@ -1,106 +1,15 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./AuthContext";
-import LoginPage from "./pages/loginPage";
-import SuccessPage from "./pages/successPage";
-import { ProtectedRoute, RedirectIfLoggedIn } from "./ProtectedRoute";
-import TailwindTest from "./pages/TailwindTest";
 import UserPage from "./pages/userpage";
-import TaskPage from "./pages/taskPage";
-import TelesaPage from "./pages/telesaPage";
-import AboutPage from "./pages/aboutPage";
-import { GlobalProvider } from "./Global"; // Používáme pojmenovaný export
-import Profile from "./Components/Profile";
-import PomocPage from "./pages/pomocPage";
 
 function App() {
   return (
-    <AuthProvider>
-      <GlobalProvider>
-        <Router>
-          <Routes>
-            {/* Public route for login */}
-            <Route path="/prihlaseni" element={<LoginPage />} />
-
-            {/* Protected routes */}
-            <Route
-              path="/uzivatel"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <SuccessPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/pomoc"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <PomocPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/tailwind"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <TailwindTest />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/ukoly"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <TaskPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/telesa"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <TelesaPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/projekt"
-              element={
-                <ProtectedRoute redirectTo="/">
-                  <AboutPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/profil"
-              element={
-                <ProtectedRoute redirectTo="/prihlaseni">
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/success"
-              element={
-                <ProtectedRoute redirectTo="/">
-                  <SuccessPage />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Redirect logged-in users from the root ("/") */}
-            <Route
-              path="/"
-              element={
-                <RedirectIfLoggedIn redirectTo="/success">
-                  <UserPage />
-                </RedirectIfLoggedIn>
-              }
-            />
-          </Routes>
-        </Router>
-      </GlobalProvider>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        {/* Public route for login */}
+        <Route path="/" element={<UserPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- NaucSeTelesa/src/pages/userpage.jsx
+++ NaucSeTelesa/src/pages/userpage.jsx
@@ -1,27 +1,8 @@
-import "../App.css";
-import Footer from "../Components/Footer";
-import Overused from "../Components/Overused";
-import Work from "../Components/Work";
-import Robot from "../Components/Robot";
-import Project from "../Components/Project";
-
 function UserPage() {
   return (
-    <>
-      <Robot />
-
-      <div className="border-t border-gray-700 min-h-screen bg-gradient-to-br from-black via-zinc-900  to-black text-white ">
-        <Overused />
-
-        <Project />
-
-        <div className="md:mt-20 md:pb-20">
-          <Work />
-        </div>
-      </div>
-
-      <Footer />
-    </>
+    <div>
+      <h1 className="text-white bg-black">User Page</h1>
+    </div>
   );
 }
 
